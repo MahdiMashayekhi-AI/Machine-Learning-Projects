f22b9e337bc2b6a27aa6ccc519da10155e7fcb2d
Mahdi Mashayekhi <mahdimashayekhi.ai@gmail.com>
2026-02-13 12:49:43 +0330

    Add other structure of detection section.

--- 05-deep-learning/Irananin-License-Plate-Recognition/main.py
+++ 05-deep-learning/Irananin-License-Plate-Recognition/main.py
@@ -1,10 +1,37 @@
 import os
-import re
-import json
-import torch
 from pathlib import Path
-from core.ocr.data.lablel_convertor import LabelConvertor
+
+ALLOWED_EXTENSIONS = {
+    ".py", ".yaml", ".yml", ".json", ".md"
+}
+
+IGNORE_DIRS = {
+    ".git",
+    "__pycache__",
+    ".idea",
+    ".vscode",
+    "node_modules"
+}
+
+def print_tree(start_path):
+    start_path = Path(start_path)
+
+    for root, dirs, files in os.walk(start_path):
+        dirs[:] = [d for d in dirs if d not in IGNORE_DIRS]
+
+        level = Path(root).relative_to(start_path).parts
+        indent = "│   " * len(level)
+        folder_name = Path(root).name if level else start_path.name
+
+        print(f"{indent}📂 {folder_name}")
+
+        sub_indent = "│   " * (len(level) + 1)
+
+        for file in files:
+            ext = Path(file).suffix.lower()
+            if ext in ALLOWED_EXTENSIONS:
+                print(f"{sub_indent}📄 {file}")
+
 
 if __name__ == "__main__":
-  
-  pass
+    print_tree(".")
