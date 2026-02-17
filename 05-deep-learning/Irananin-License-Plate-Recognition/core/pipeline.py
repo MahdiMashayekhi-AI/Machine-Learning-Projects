729236e2fb68c28c2d0f2bc3b0fad335f5c90fce
Mahdi Mashayekhi <mahdimashayekhi.ai@gmail.com>
2026-02-17 16:07:45 +0330

    Write core pipeline for using in tools

---
+++ 05-deep-learning/Irananin-License-Plate-Recognition/core/pipeline.py
@@ -0,0 +1,48 @@
+from core.entities.pipeline import PipelineResult
+
+class Pipeline:
+  def __init__(self, det, ocr, threshold=0.25):
+    self.det = det
+    self.ocr = ocr
+    self.threshold = threshold
+
+  def run(self, image) -> list[PipelineResult]:
+    outputs: list[PipelineResult] = []
+    detections = self.det.predict(image, conf=self.threshold)
+
+    if len(detections) > 0:
+      for det in detections:
+        if det.conf > self.threshold:
+
+          cropped_img = self._cropper(image, det.bbox)
+          if cropped_img is None:
+            continue
+
+          ocr_result = self.ocr.predict(cropped_img)
+          if not ocr_result.text:
+            continue
+
+          outputs.append(PipelineResult(
+            bbox=det.bbox,
+            text=ocr_result.text,
+            det_conf=det.conf,
+            ocr_conf=None)
+          )
+
+    outputs.sort(key=lambda x: x.det_conf, reverse=True)
+    return outputs
+  
+
+  def _cropper(self, image, bbox):
+    h, w = image.shape[:2]
+    x1, y1, x2, y2 = bbox
+
+    x1 = int(max(x1, 0))
+    y1 = int(max(y1, 0))
+    x2 = int(min(x2, w))
+    y2 = int(min(y2, h))
+    
+    if x2 <= x1 or y2 <= y1:
+      return None
+    
+    return image[y1:y2, x1:x2]
