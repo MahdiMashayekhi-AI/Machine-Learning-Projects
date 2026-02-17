8b3f45f328ca045c621c555866d1a4ee00a171da
Mahdi Mashayekhi <mahdimashayekhi.ai@gmail.com>
2026-02-17 16:04:02 +0330

    Delete device logic from config and fix typo

--- 05-deep-learning/Irananin-License-Plate-Recognition/configs/ocr.py
+++ 05-deep-learning/Irananin-License-Plate-Recognition/configs/ocr.py
@@ -6,13 +6,12 @@ IMAGE_SIZE = (200, 50)
 BATCH_SIZE = 64
 EPOCHS = 25
 LR = 1e-3
-DEVICE = ("cuda" if torch.cuda.is_available() else "cpu")
 
 EXPERIMENT_ROOT = "./experiments/ocr"
 DATASET_DIR = "./data/ocr/raw"
 CLASSES_PATH = "./configs/classes.json"
 
-LOGEER_DIR = "./logs"
+LOGGER_DIR = "./logs"
 LOGGER_FILE = "result.csv"
 
 CHECKPOINT_DIR = "./checkpoints"
