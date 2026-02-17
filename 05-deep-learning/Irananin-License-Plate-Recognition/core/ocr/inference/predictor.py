73c410f0913563a91c619dc953699f8b06144bdd
Mahdi Mashayekhi <mahdimashayekhi.ai@gmail.com>
2026-02-17 16:16:11 +0330

    Fix some little problems

--- 05-deep-learning/Irananin-License-Plate-Recognition/core/ocr/inference/predictor.py
+++ 05-deep-learning/Irananin-License-Plate-Recognition/core/ocr/inference/predictor.py
@@ -1,19 +1,27 @@
 import torch
+import numpy as np
+from core.entities.ocr import OCRResult
+from core.ocr.data.transforms import preprocess_image
 
 class Predictor:
   def __init__(self, model, convertor, device):
     self.model = model
     self.convertor = convertor
     self.device = device
+    self.model.to(device)
     self.model.eval()
 
 
   @torch.no_grad()
-  def predict(self, images):
-    images = images.to(self.device)
+  def predict(self, image: np.ndarray) -> OCRResult:
+    input_tensor = preprocess_image(image).to(self.device)
 
-    logits = self.model(images)
-    
+    logits = self.model(input_tensor)
     texts = self.convertor.decode(logits)
-    return texts
+    text = texts[0] if texts else ""
+
+    return OCRResult(
+      text=text,
+      conf=None
+    )
     
