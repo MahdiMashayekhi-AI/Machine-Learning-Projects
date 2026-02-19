dc8e3d73f28a9e74b08635e0b763ac436615764f
Mahdi Mashayekhi <mahdimashayekhi.ai@gmail.com>
2026-02-19 13:14:47 +0330

    Remove function and add it in form of class

--- 05-deep-learning/Irananin-License-Plate-Recognition/core/ocr/inference/postprocess.py
+++ 05-deep-learning/Irananin-License-Plate-Recognition/core/ocr/inference/postprocess.py
@@ -1,11 +1,26 @@
-def postprocess(texts):
-  results = []
+import re
+from core.entities.ocr import OCRResult
 
-  for t in texts:
-    t = t.strip()
-    t = t.replace(" ", "")
-    t = t.replace("_", "")
 
-    results.append(t)
+class PlateValidator:
+  def __init__(self):
+    self._pattern = re.compile(r"^[1-9][0-9][ابپتثجدزژسشصطعقلمنوهی][1-9][0-9]{2}[1-9][0-9]$")
 
-  return results
+  
+  def normalize(self, text):
+    results = ""
+
+    for t in text:
+      t = t.strip()
+      t = t.replace(" ", "")
+      t = t.replace("_", "")
+
+      results += t
+
+    return results
+
+
+  def validate(self, ocr_result: OCRResult):
+    clean_text = self.normalize(ocr_result.text)
+
+    return self._pattern.match(clean_text)
