c1368a00f352d54b3c902b50613bfc8b3082096f
Mahdi Mashayekhi <mahdimashayekhi.ai@gmail.com>
2026-02-19 13:13:27 +0330

    Edit name of the class and calculate the conf score

--- 05-deep-learning/Irananin-License-Plate-Recognition/core/ocr/inference/predictor.py
+++ 05-deep-learning/Irananin-License-Plate-Recognition/core/ocr/inference/predictor.py
@@ -3,7 +3,7 @@ import numpy as np
 from core.entities.ocr import OCRResult
 from core.ocr.data.transforms import preprocess_image
 
-class Predictor:
+class OCRPredictor:
   def __init__(self, model, convertor, device):
     self.model = model
     self.convertor = convertor
@@ -17,11 +17,25 @@ class Predictor:
     input_tensor = preprocess_image(image).to(self.device)
 
     logits = self.model(input_tensor)
+    probs = torch.softmax(logits, dim=2)
+    max_probs, max_indices = probs.max(dim=2)
+
     texts = self.convertor.decode(logits)
     text = texts[0] if texts else ""
 
+    confidence = []
+    prev = -1
+    for i, idx in enumerate(max_indices[0]):
+      idx = idx.item()
+      if idx != prev and idx != 0:
+        confidence.append(max_probs[0][i].item())
+      prev = idx
+
+    conf = sum(confidence) / len(confidence) if confidence else 0.0
+
+
     return OCRResult(
       text=text,
-      conf=None
+      conf=conf,
     )
     
